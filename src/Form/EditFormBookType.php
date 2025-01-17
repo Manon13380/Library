bd590c43f5379767a850deea0d659e52f06f00a5
manon13380 <manon.carpentieri@hotmail.com>
2025-01-17 16:35:37 +0100

    fix bug modify genre

--- src/Form/EditFormBookType.php
+++ src/Form/EditFormBookType.php
@@ -3,11 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Book;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\CallbackTransformer;
 
 class EditFormBookType extends AbstractType
 {
@@ -26,9 +29,32 @@ class EditFormBookType extends AbstractType
             ->add('description', null, [
                 'label' => 'Description'
             ])
-            ->add('genre', null, [
-                'label' => 'Genre'
+            ->add('genre', TextType::class, [
+                'label' => 'Genre',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Genres (séparés par des virgules)'
+                ]
             ])
+        ;
+
+        $builder->get('genre')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($genreAsArray) {
+                    // transform the array to a string
+                    return $genreAsArray ? implode(', ', $genreAsArray) : '';
+                },
+                function ($genreAsString) {
+                    // transform the string back to an array
+                    if (!$genreAsString) {
+                        return [];
+                    }
+                    $genres = array_map('trim', explode(',', $genreAsString));
+                    return array_filter($genres, function($genre) {
+                        return !empty($genre);
+                    });
+                }
+            ))
         ;
     }
 
